89b04cd95e51911173056e48f656e2199cb61494
Pat Heard <patrick.heard@cds-snc.ca>
2021-07-25 22:24:00 +0000

    feat: add Terraform to create remote tfstate bucket

---
+++ .bootstrap/tfstate.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.50.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ca-central-1"
+}
+
+variable "tfstate_bucket_name" {
+  description = "Name of the S3 bucket to use to store the tfstate"
+  type        = string
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = var.tfstate_bucket_name
+
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tfstate_public_access_block" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
